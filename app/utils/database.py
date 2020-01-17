c1efe991483a40ed0e2e93904172aca5dbd3cd7e
Dmitrii <dmitrij.djachkov@yandex.ru>
2020-01-17 16:08:43 +0300

    Add a simple mongoDB engine, enable tests

--- app/utils/database.py
+++ app/utils/database.py
@@ -1,21 +1,35 @@
 from pymongo import MongoClient
 from datetime import datetime
+from bson.objectid import ObjectId
 from os import environ
 
 
 class Database:
     client = MongoClient(environ.get("MONGO_HOSTNAME") or "localhost", 27017)
-    db = client[environ.get("MONGO_DB") or "test"]
+    db = client[environ.get("MONGO_DB") or "testtest"]
     collection = db[environ.get("MONGO_COLLECTION") or "test"]
+
+    @staticmethod
+    def __serialize_id(_id):
+        return ObjectId(_id)
 
     def insert(self, doc):
         return str(self.collection.insert_one(doc).inserted_id)
 
     def get(self, _id, projection=None):
+        _id = self.__serialize_id(_id)
         return self.collection.find_one(_id, projection=projection)
 
     def get_all(self, projection=None):
         return [doc for doc in self.collection.find()]
+
+    def update(self, _id, data):
+        _id = self.__serialize_id(_id)
+        return self.collection.update_one({"_id": _id}, {"$set": data}).acknowledged
+
+    def delete(self, _id):
+        _id = self.__serialize_id(_id)
+        return self.collection.delete_one({"_id": _id}).acknowledged
 
 
 test_doc = {
@@ -26,4 +40,5 @@ test_doc = {
 
 if __name__ == "__main__":
     db = Database()
-    print(db.insert(test_doc))
+    a = db.insert(test_doc)
+    print(db.update(a, {"text": "updated sample text"}))
---
+++ tests/database_test.py
@@ -0,0 +1,27 @@
+import pytest
+from app.utils.database import Database
+
+def pytest_namespace():
+    return {"_id": None}
+class TestDatabase:
+    db = Database()
+    test_doc = {
+        "author": "Dmitriy Diachkov",
+        "text": "Sample text",
+    }
+
+
+    def test_insert(self):
+        pytest._id = self.db.insert(self.test_doc)
+        assert len(pytest._id) == 24
+
+    def test_get(self):
+        assert self.db.get(pytest._id) == self.test_doc
+
+    def test_update(self):
+        updated = self.db.update(pytest._id, {"text": "Updated sample text"})
+        assert updated is True
+
+    def test_delete(self):
+        deleted = self.db.delete(pytest._id)
+        assert deleted is True
